5efbf624bbe01bd162236c86f41ead24beea5682
Dan Plubell <dplubell@cerner.com>
2016-01-05 16:20:17 -0600

    add uid dictionary lookup and load

--- src/Data/DICOM/Dictionary.hs
+++ src/Data/DICOM/Dictionary.hs
@@ -1,11 +1,17 @@
 module Data.DICOM.Dictionary
        ( lookupElementByTag
        , loadElementDictionary
+       , loadUIDDictionary
+       , lookupUIDType
+       , lookupUID
        ) where
 
 
+import qualified Data.Bimap as DB
+import qualified Data.ByteString as DBS
 import Data.DICOM.Dictionary.Internal.InternalDictionary
 import Data.DICOM.Model.Dictionary
+import Data.DICOM.Model.RegisteredUID
 import qualified Data.Map as DM
 import Data.Word
 
@@ -16,3 +22,12 @@ lookupElementByTag dd tag = DM.lookup tag dd
 -- | Load the dicom element dictionary
 loadElementDictionary::DicomDictionary
 loadElementDictionary = DM.fromList dictElementList
+
+loadUIDDictionary::UIDDictionary
+loadUIDDictionary= DB.fromList dicomUIDTable
+
+lookupUIDType::UIDDictionary -> DBS.ByteString -> DicomUID
+lookupUIDType = (DB.!)
+
+lookupUID::UIDDictionary -> DicomUID -> DBS.ByteString
+lookupUID = (DB.!>)
